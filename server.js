edbbf604a6e6e9757bccfde6453707bbd5bfb756
Clay O'Dell <clayodell.swe@gmail.com>
2025-05-20 22:41:29 -0500

    updated config and server files

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const app = require("./app");
 const config = require("./config");
+const pool = require("./db");
 
 const PORT = config.port || 5000;
+pool.connect((err) => {
+    if (err) {
+        console.error('Failed to connect to the database', err);
+        process.exit(1);
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    } else {
+        console.log('Connected to the database');
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT ${PORT}`);
+    });
 });
